c1d48836c0f643770d183d41d48e9cc2206aa96e
John Robinson <johnrobinsn@gmail.com>
2024-12-06 01:05:45 -0500

    experiment with streaming

--- openai_agent/openaiasync.py
+++ openai_agent/openaiasync.py
@@ -13,15 +13,35 @@ openai_api_key = config.get('openai_api_key')
 
 client = AsyncOpenAI(api_key=openai_api_key)
 
+# sync version
+# async def prompt(messages):
+#     start = datetime.now()
+#     response = await client.chat.completions.create(
+#         model="gpt-4-1106-preview",
+#         messages=messages,
+#     )
+#     log.info('Time for LLM Response: %d', (datetime.now()-start).total_seconds())
+#     r = response.choices[0].message.content
+#     print('openai response:', r)
+#     return r
+
+# streaming version
 async def prompt(messages):
     start = datetime.now()
     response = await client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=messages,
+        stream=True,
     )
-    log.info('Time for LLM Response: %d', (datetime.now()-start).total_seconds())
-    r = response.choices[0].message.content
-    print('openai response:', r)
+    # log.info('Time for LLM Response: %d', (datetime.now()-start).total_seconds())
+    # r = response.choices[0].message.content
+    # print('openai response:', r)
+    r = ''
+    async for chunk in response:
+        print(chunk)
+        print(chunk.choices[0].delta.content)
+        if chunk.choices[0].delta.content:
+            r = r + chunk.choices[0].delta.content
     return r
 
 def startWorker():
